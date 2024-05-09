9305a1e3d1859ce08c525b675a0ba00c57e555ed
Vegas-in <angeldevegas@gmail.com>
2024-05-09 16:22:41 +0200

    require cors morgan dotenv

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
@@ -7,17 +9,26 @@ const userRoutes = require("./routes/user.routes")
 
 //Middlewares
 app.use(express.json()); // Para parsear el body de las peticiones
+const error404 = require("./middlewares/error404");
+const morgan = require("./middlewares/morgan");
+
+app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
+
+app.use(cors());
 
 /******RUTAS ******/
 
 // http://localhost:3000/
 app.get("/", (req, res) => {
-  res.status(200).send("");
+  res.status(200).send("Estas en el Home");
 });
 
 //API
 app.use('/api/user',userRoutes);
 
+
+//app.use(error404);
+app.use("*", error404); // Middleware que gestiona el error 404
 
 // http://localhost:3000
 app.listen(port, () => {
